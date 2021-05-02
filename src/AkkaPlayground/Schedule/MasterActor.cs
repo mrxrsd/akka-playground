7426a01a0b8c4adc18ef863d983101ae114e34b8
mrxrsd <mrxrsd@gmail.com>
2021-05-02 17:04:38 -0300

    schedule message

---
+++ src/AkkaPlayground/Schedule/MasterActor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Akka.Actor;
+using Core.Messages.Common;
+
+namespace Schedule
+{
+    public class MasterActor : ReceiveActor
+    {
+
+        public MasterActor()
+        {
+            Context.System.Scheduler.ScheduleTellRepeatedly(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2), Context.Self, new WakeUp(""), Self);
+
+            Receive<WakeUp>(up =>
+            { 
+                Console.WriteLine("Hi!");
+            });
+        }
+    }
+}
---
+++ src/AkkaPlayground/Schedule/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Akka.Actor;
+
+namespace Schedule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sys = ActorSystem.Create("sys");
+            var master = sys.ActorOf(Props.Create<MasterActor>(), "master");
+
+
+            Console.ReadKey();
+        }
+    }
+}
