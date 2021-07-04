ada35112c3fab5ec8f7a31af1fe67c55b4b431bf
mrxrsd <mrxrsd@gmail.com>
2021-07-04 20:16:05 -0300

    Hocon Substitution

--- src/AkkaPlayground/Core/ConfigurationLoader.cs
+++ src/AkkaPlayground/Core/ConfigurationLoader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using Akka.Configuration;
 
@@ -8,13 +9,23 @@ namespace Core
 {
     public static class ConfigurationLoader
     {
-        public static Config Load() => LoadConfig("akka.conf");
+        public static Config Load(Dictionary<string,string> vals = null) => LoadConfig("akka.conf", vals);
 
-        private static Config LoadConfig(string configFile)
+        public static Config LoadConfig(string configFile, Dictionary<string,string> vals)
         {
             if (File.Exists(configFile))
             {
                 string config = File.ReadAllText(configFile);
+                if (vals != null && vals.Any())
+                {
+                    config += "\n";
+                    foreach (var val in vals)
+                    {
+                        config += $"{val.Key}=\"{val.Value}\"\n";
+                    }
+            
+                }
+
                 return ConfigurationFactory.ParseString(config);
             }
 
---
+++ src/AkkaPlayground/HoconSubstitution/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Core;
+
+namespace HoconSubstitution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dic = new Dictionary<string, string>
+            {
+                {"akka.persistence.journal.redis.connection-string", "injected in runtime"}
+
+            };
+
+            var config = ConfigurationLoader.LoadConfig("akka.conf", dic);
+            Console.WriteLine($"{config.GetValue("akka.persistence.journal.redis.connection-string")}");
+                
+            Console.ReadKey();
+        }
+    }
+}
